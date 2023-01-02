f4f2bbaac77e6a99d7dc36319ae630c9fb4503c7
Kun Wu <kun.wu@eyevinn.se>
2023-01-02 06:19:32 +0100

    remove redundant data

--- src/lfu_cache.rs
+++ src/lfu_cache.rs
@@ -6,40 +6,47 @@ use std::hash::Hash;
 use std::marker::PhantomData;
 
 pub struct LfuUniCache<T: Clone + Debug + Hash + Eq> {
-    lfu_cache: LfuCache<T, PhantomData<bool>>,
+    capacity: usize,
+    lfu_cache: LfuCache<usize, PhantomData<bool>>,
     index_cache: IndexMap<T, PhantomData<bool>>,
 }
 
 impl<T: Clone + Debug + Hash + Eq> UniCache<T> for LfuUniCache<T> {
     fn new(capacity: usize) -> Self {
         Self {
+            capacity,
             lfu_cache: LfuCache::with_capacity(capacity),
             index_cache: IndexMap::with_capacity(capacity),
         }
     }
 
     fn put(&mut self, item: T) {
-        let _maybe_evicted = self.lfu_cache.insert(item.clone(), PhantomData);
-        // TODO remove maybe_evicted item from index_cache
-        self.index_cache.insert(item, PhantomData);
+        let (index, _) = self.index_cache.insert_full(item, PhantomData);
+        if self.lfu_cache.insert(index, PhantomData).is_some() {
+            // already exists
+            return;
+        }   // not exists so insert it and check if full
+        else if self.lfu_cache.len() > self.capacity {
+            // full, pop out one item
+            self.lfu_cache.pop_lfu_key_value();
+        }
     }
 
     fn get_encoded_index(&mut self, item: &T) -> Option<usize> {
-        if self.lfu_cache.get(item).is_some() {
-            self.index_cache.get_index_of(item)
-        } else {
-            None
+        if let Some(index) = self.index_cache.get_index_of(item) {
+            // Have seen it before
+            if self.lfu_cache.get(&index).is_some() {
+                // exists in cache
+                return Some(index);
+            }
+            // not in cache, already popped out
         }
+
+        None
     }
 
     fn get_with_encoded_index(&mut self, index: usize) -> T {
         let item = self.index_cache.get_index(index).unwrap().0.clone();
-        self.lfu_cache.get(&item).unwrap_or_else(|| {
-            panic!(
-                "Tried to update frequency of cache item that doesn't exist: {:?}",
-                &item
-            )
-        });
         item
     }
 }
--- src/lru_cache.rs
+++ src/lru_cache.rs
@@ -7,37 +7,48 @@ use std::marker::PhantomData;
 use std::num::NonZeroUsize;
 
 pub struct LruUniCache<T: Clone + Debug + Hash + Eq> {
-    lru_cache: LruCache<T, PhantomData<bool>>,
+    capacity: usize,
+    lru_cache: LruCache<usize, PhantomData<bool>>,
     index_cache: IndexMap<T, PhantomData<bool>>,
 }
 
 impl<T: Clone + Debug + Hash + Eq> UniCache<T> for LruUniCache<T> {
     fn new(capacity: usize) -> Self {
         Self {
+            capacity,
             lru_cache: LruCache::new(NonZeroUsize::new(capacity).unwrap()),
             index_cache: IndexMap::with_capacity(capacity),
         }
     }
 
     fn put(&mut self, item: T) {
-        let maybe_evicted = self.lru_cache.push(item.clone(), PhantomData);
-        if let Some((evicted, _)) = maybe_evicted {
-            let _ = self.index_cache.remove(&evicted);
+
+        let (index, _) = self.index_cache.insert_full(item, PhantomData);
+        if self.lru_cache.push(index, PhantomData).is_some() {
+            // already exists
+            return;
+        }   // not exists so insert it and check if full
+        else if self.lru_cache.len() > self.capacity {
+            // full, pop out one item
+            self.lru_cache.pop_lru();
         }
-        self.index_cache.insert(item, PhantomData);
     }
 
     fn get_encoded_index(&mut self, item: &T) -> Option<usize> {
-        if self.lru_cache.get(item).is_some() {
-            self.index_cache.get_index_of(item)
-        } else {
-            None
+        if let Some(index) = self.index_cache.get_index_of(item) {
+            // Have seen it before
+            if self.lru_cache.get(&index).is_some() {
+                // exists in cache
+                return Some(index);
+            }
+            // not in cache, already popped out
         }
+
+        None
     }
 
     fn get_with_encoded_index(&mut self, index: usize) -> T {
         let item = self.index_cache.get_index(index).unwrap().0.clone();
-        self.lru_cache.promote(&item);
         item
     }
 }
